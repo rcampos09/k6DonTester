d216ef1aa146406029508500e6374bc0c6d43e50
Rodrigo Campos <rodrigo.campos@hites.cl>
2021-02-16 02:29:57 -0300

    update structure folder and json dashboard

--- k6/scenario/scenarioTest1.js
+++ k6/scenario/scenarioTest1.js
@@ -9,9 +9,15 @@ import {homePage} from '../services/homePage.js'
 import {loginPagePOST, loginPageGET, logoutPagePOST} from '../services/loginPage.js'
 
 export let options = {
-    vus: 1,
-    duration: '1s',
-};
+    stages: [
+        // Ramp-up from 1 to 5 VUs in 5s
+        { duration: "5s", target: 5 },
+        // Stay at rest on 5 VUs for 10s
+        { duration: "10s", target: 5 },
+        // Ramp-down from 5 to 0 VUs for 5s
+        { duration: "5s", target: 0 }
+    ]
+  };
 
 export default function () {
     let token;
